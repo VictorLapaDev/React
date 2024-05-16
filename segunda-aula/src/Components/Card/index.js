158d1203ff2fa9cec72f923d0ef382736233839c
VictorLapaDev <victorlucaslapa@gmail.com>
2024-05-16 18:49:42 -0300

    Feat: update project

--- segunda-aula/src/Components/Card/index.js
+++ segunda-aula/src/Components/Card/index.js
@@ -5,7 +5,7 @@ import tempo from "../../Assets/tempo.png"
 function Card(props){
     return(
         <div className="card">
-            <Titulo>22º</Titulo>
+            <Titulo texto="22º"></Titulo>
             <img src={tempo} alt="img" className="sol"></img>
             <h4 className="estado"><code>{props.diasSemana}</code></h4>
         </div>
